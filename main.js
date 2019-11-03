d1a10e08929097ae766676d1d34558c5e8c8ae00
violettagoldman <violagoldman@mail.ru>
2019-11-03 22:17:19 +0100

    Beginning of front

---
+++ main.js
@@ -0,0 +1,24 @@
+const N = 4;
+var grid = [];
+var boards = [];
+
+function setup()
+{
+  createCanvas(500, 500);
+}
+
+function draw()
+{
+  background(0);
+  drawGrid();
+}
+
+function loadBoards()
+{
+  
+}
+
+function drawGrid()
+{
+
+}
